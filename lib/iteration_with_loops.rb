519101bb4d31a4cbf1a2c9f6b43a3b4d15c22b09
Obinna Nwabia <valinevite@gmail.com>
2020-11-17 12:03:52 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,9 @@ def find_even_values(src)
 count = 0
 while count < src.length do
   inner_count = 0
-  while inner_count < srr[count][inner_count].length do
-  
+  while inner_count < src[count].length && src[count][inner_count] % 2 == 0 do
+    p src[count][inner_count]
+  inner_count += 1
 end
+ count += 1
 end
